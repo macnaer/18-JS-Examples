9b9f7eb6aa888c747bf0efafa774303dd297d090
master <master@rivne.itstep.org>
2021-05-19 20:01:49 +0300

    dom example

--- 19.05.21/js/common.js
+++ 19.05.21/js/common.js
@@ -24,12 +24,63 @@ function GetCar() {
     price.value = "";
 
     CarsArr.push(Car)
+    Render();
 
-    console.clear();
+    // console.clear();
 
-    CarsArr.forEach(car => {
-        car.Print();
-    })
+    // CarsArr.forEach(car => {
+    //     car.Print();
+    // })
 
-    console.log(CarsArr.length);
+    // console.log(CarsArr.length);
 }
+
+
+function Render() {
+    const root = document.getElementById("root");
+    let span = document.createElement("span");
+    span.setAttribute("class", "test");
+    span.innerHTML = "Some text";
+    root.appendChild(span);
+}
+
+// Declaration
+// function Plus(a, b) {
+//     console.log(a + b);
+// }
+// // Sum(4, 6);
+
+
+// // Expression
+// const Minus = function (a, b) {
+//     console.log(a - b);
+// }
+
+// // Minus(20, 5);
+
+// // Arrow
+// const Multiply = (a, b) => {
+//     console.log(a / b);
+// }
+
+// Multiply(10, 5);
+
+// Annonyumous
+// (function (a, b) {
+//     console.log(a * b);
+// }(20, 5));
+
+// function Car(vendor, model, price) {
+//     this.vendor = vendor;
+//     this.model = model;
+//     this.price = price;
+//     this.Print = function () {
+//         console.log(`${this.vendor}\n${this.model}\n${this.price}`)
+//     }
+// }
+
+// const Audi = new Car("Audi", "A8", 12000);
+// Audi.Print();
+
+// const BMW = new Car("BMW", "320", 11000);
+// BMW.Print();
